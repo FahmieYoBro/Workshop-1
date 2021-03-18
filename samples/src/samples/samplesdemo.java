75c69b032a456c5980eea6a1c30fbffcce5f2306
FahmieYoBro <fyb7920@gmail.com>
2021-03-18 16:02:15 +0800

    Initial Commit

--- samples/src/samples/samplesdemo.java
+++ samples/src/samples/samplesdemo.java
@@ -5,7 +5,7 @@ public class samplesdemo {
 	public static void main(String[] args) {
 		System.out.print("Git is fun");
 		System.out.print("Git is cool");
-		System.out.print("CB KAU");
+
 		
 	}
 }

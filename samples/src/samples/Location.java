058c07c12635b703ef5cba87b4319a3cb5426613
FahmieYoBro <fyb7920@gmail.com>
2021-03-18 16:06:30 +0800

    First Commit Location

---
+++ samples/src/samples/Location.java
@@ -0,0 +1,14 @@
+package samples;
+
+public class Location {
+	private String name;
+	private int locationid;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
